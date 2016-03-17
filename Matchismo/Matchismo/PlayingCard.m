6604668e785512f51131a7c8f8e0666ba27542e0
Matt Hayden (ios) <matt.hayden@mcmaster.com>
2016-03-17 12:15:17 -0500

    supporting classes before UI

--- Matchismo/Matchismo/PlayingCard.m
+++ Matchismo/Matchismo/PlayingCard.m
@@ -11,8 +11,35 @@
 @implementation PlayingCard
 
 - (NSString *) contents{
-    NSArray *rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+    NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
+
++ (NSArray *) validSuits {
+    return @[@"♥️",@"♦️",@"♠️",@"♣️"];
+}
+
++ (NSArray *)rankStrings{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRank {return [[self rankStrings] count]-1;}
+
+- (void)setRank:(NSUInteger)rank {
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
+
+@synthesize suit = _suit;
+- (void) setSuit:(NSString *) suit {
+    if ([PlayingCard validSuits] containsObject:suit]){
+        _suit = suit;
+    }
+}
+- (NSString *) suit {
+    return _suit ? _suit : @"?";
+}
+         
 
 @end
---
+++ Matchismo/Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by mobile on 3/17/16.
+//  Copyright (c) 2016 McMaster-Carr. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
