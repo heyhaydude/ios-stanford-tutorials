14cff7e20bdb83797a50f8d13cbe33acd7c191e6
Matt Hayden (ios) <matt.hayden@mcmaster.com>
2016-03-18 11:52:51 -0500

    finished homework 2

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,21 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by mobile on 3/18/16.
+//  Copyright (c) 2016 McMaster-Carr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck;
+- (void) chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic,readonly) NSInteger score;
+
+@end
